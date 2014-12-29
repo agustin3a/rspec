1a0cc453ecd6d96240a50cc0a05e6239e0723f70
Carlos Aguilar <agustin3a94@gmail.com>
2014-12-28 19:48:47 -0600

    Testing rails controllers

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,5 +1,126 @@
 require 'rails_helper'
+require "spec_helper"
+require 'rspec/its'
+require 'rspec/collection_matchers'
 
-RSpec.describe LocationsController, :type => :controller do
-
+describe LocationsController do
+	describe "#create" do
+		subject { post :create, { :location => 
+			{ :latitude => 25.0 ,
+				:longitude => -40.0 }
+			}
+		}
+		it "saves the location" do 
+			subject
+			Location.all.count.should == 1
+		end
+		it "should redirect to show the created location" do
+			subject.should redirect_to(location_path(Location.first.id))
+		end
+	end 
+	describe "#new" do
+		context "when invalid longitude" do
+			subject { post :create, { :location => 
+				{ :latitude => 25.0 } } }
+			its(:status) { should == 200 }
+			it "should render the new view" do
+				subject
+				response.should render_template("new")
+			end
+		end
+	end
+	describe "#show" do
+		context "when the location exists" do
+			let(:location) { Location.create(
+				:latitude => 25.0, :longitude => -40.0)
+			}
+			subject { get :show, :id => location.id }
+			it "assigns @location" do
+				subject
+				assigns(:location).should eq(location)
+			end
+			it "renders the show template" do
+				subject
+				response.should render_template("show")
+			end
+		end
+		context "when the location does not exist" do
+			subject { get :show, :id => 404 }
+			its(:status) { should == 404}
+		end
+	end
+	describe "#index" do
+		context "when there some locations" do
+			let(:locations) do
+				[
+					Location.create(:latitude => 25.0,
+					:longitude => -40.0),
+					Location.create(:latitude => -10.0,
+					:longitude => 42.0)
+				]
+			end
+			before { locations }
+			subject { get :index }
+			it "assigns @locations" do
+				subject 
+				assigns(:locations).should eq(locations)
+			end
+		end
+		context "when there are no locactions" do
+			subject { get :index }
+			it "assings @locations" do
+				subject
+				assigns(:locations).should eq([])
+			end
+		end
+	end
+	describe "#destroy" do
+		context "when the location exists" do
+			let(:location) { Location.create(
+				:latitude => 25.0, :longitude => -40.0)
+			}
+			subject { post :destroy, :id => location.id }
+			it "deletes the location" do
+				subject
+				Location.all.count.should == 0
+			end 
+		end 
+		context "when the location does not exist" do
+			subject { post :destroy, :id => 404}
+			its(:status) { should == 404}
+		end	
+	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
